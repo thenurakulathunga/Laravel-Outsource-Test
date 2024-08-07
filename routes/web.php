8ce2e9ea4e604c89f4f1f49cb50ee476077cf081
Thenura Kulathunga <thenura@thesanmark.com>
2024-08-07 09:30:36 +0530

    change routes to spatie middlewares

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::group(['middleware' => ['auth', 'role:Admin']], function () {
+    Route::view('dashboard', 'dashboard')
+        ->middleware(['verified'])
+        ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('profile', 'profile')
+        ->name('profile');
+});
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
